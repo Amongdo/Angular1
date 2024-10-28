9c76821be20e7f4a80599c31b9bc1e9a8ab9d150
Byungdo Ahn <dksqudeh2@gmail.com>
2024-10-28 17:29:01 +0900

    인생 첫 앵귤러

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem{
+  id:number;
+  task:string;
+  completed:boolean
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Amongdo';
+ 
+  todoList:TodoItem [] = [];
+  newTask:string = ''
+  
 }
